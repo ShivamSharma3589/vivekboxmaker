99cfd1c18edc5dbb6dd5402aae744276b918c74c
Shivam Sharma <shivayasharma1149@gmail.com>
2024-12-22 09:31:44 -0800

    Installed bootstrap SCSS version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
+import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header>Header</header>
+      <Component {...pageProps} />
+      <footer>Footer</footer>
+    </>
+  );
 }
